db9644fbb597b367353da1b087611b8f420b10f0
Shaun Mulligan <switchflip.shaun@gmail.com>
2015-10-28 14:32:13 +0000

    add soundmachine code

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ app.on('ready', function() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/app/index.html');
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
