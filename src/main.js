46bc7e19acb12e35ba3bcc86e04f778dc5c8837b
Pablo Carranza Vélez <pablocarranza@gmail.com>
2015-11-11 15:04:28 -0300

    Configure ALSA and add calibration

--- src/main.js
+++ src/main.js
@@ -31,7 +31,7 @@ app.on('ready', function() {
     kiosk: true,
   });
 
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/build/index.html');
